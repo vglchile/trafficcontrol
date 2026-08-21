c794c42556ac92288334a62cd0804f0d61d0c761
C Guajardo <cguajardo@vgl.cl>
2026-08-20 22:51:56 -0400

    Mapper Adaptability Hotfix

--- lib/go-atscfg/parentdotconfig.go
+++ lib/go-atscfg/parentdotconfig.go
@@ -352,7 +352,7 @@ func MakeParentDotConfig(
 						// This behavior depends on a round_robin option. It will be enabled in a future version of Mapper.
 						// insertionProxySuffix = "\" secondary_parent=\"" + rule.OriginFQDN + ":" + rule.OriginPort + "\" secondary_mode=2 go_direct=false parent_is_proxy=true"
 					}
-					mapperLine := "dest_domain=" + mapperDestDomain + prefixLineNoPlaylist + " suffix=.m3u8" + " scheme=" + rule.RequestScheme + " parent=\"" + insertionParent + insertionProxySuffix
+					mapperLine := "dest_host=" + mapperDestDomain + prefixLineNoPlaylist + " suffix=.m3u8" + " scheme=" + rule.RequestScheme + " parent=\"" + insertionParent + insertionProxySuffix
 					// mapperLine += "url_regex=" + "m3u8" + " path=" + rule.OriginPathNoPlaylist + " scheme=https parent=\"" + insertionParent + "\" secondary_parent=\"" + rule.OriginFQDN + ":" + rule.OriginPort + "\" secondary_mode=2 go_direct=true" + insertionProxySuffix + "\n"
 					if !mappingExists(mapperParentRawLines, mapperLine) {
 						mapperParentRawLines = append(mapperParentRawLines, mapperLine)
@@ -366,7 +366,7 @@ func MakeParentDotConfig(
 				if !rule.BackupIsProxy {
 					backupProxySuffix = " parent_is_proxy=false"
 				}
-				mapperLine := "dest_domain=" + mapperDestDomain + prefixLineNoPlaylist + " scheme=" + rule.RequestScheme + " parent=\"" + backupParent + "\" go_direct=true" + backupProxySuffix + ignoreSelfDetect
+				mapperLine := "dest_host=" + mapperDestDomain + prefixLineNoPlaylist + " scheme=" + rule.RequestScheme + " parent=\"" + backupParent + "\" go_direct=false" + backupProxySuffix + ignoreSelfDetect
 				// mapperLine += "dest_host=" + rule.OriginFQDN + prefixLineNoPlaylist + " scheme=https parent=\"" + backupParent + "\" go_direct=true" + backupProxySuffix + ignoreSelfDetect + "\n"
 				if !mappingExists(mapperParentRawLines, mapperLine) {
 					mapperParentRawLines = append(mapperParentRawLines, mapperLine)
